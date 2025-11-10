c5d80a2111ca3553a954216cdaa298f9af67520a
Ayush Pratap Singh <ayushdce2@gmail.com>
2025-11-10 22:33:51 +0530

    add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import EmployeeHome from "./employee/EmployeeHome.jsx";
 import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Login from "./Login.jsx";
 function App() {
 
   return (
     <>
       <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Login/>}></Route>
         <Route path="/emp_mgmt_mern/*" element={<EmployeeHome/>}></Route>
 
       </Routes>
