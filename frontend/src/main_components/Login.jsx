800296b3dbdbc5e8dc4f7f7fc34c7f6139b5e69b
Ayush Pratap Singh <ayushdce2@gmail.com>
2025-11-21 21:56:14 +0530

    add

--- frontend/src/main_components/Login.jsx
+++ frontend/src/main_components/Login.jsx
@@ -18,8 +18,8 @@ const Login = () => {
         <div className='ml-[1vw] md:ml-[20vw] shadow-[rgba(0,0,0,0.24)_0px_3px_8px] flex flex-col w-[95%] md:w-[30%] p-7 rounded'>
             <p className='text-4xl text-[#71b1e5] leading-9 font-bold mb-8'>Sign In</p>
             <form onSubmit={loginSubmit} className='flex flex-col'>
-              <input type='text' placeholder='email' name="email" onChange={loginFormData} className='text-lg mb-5 h-10 p-2 outline-0 border-b-2 text-[#71b1e5] border-b-[#71b1e5]'/>
-              <input type='text' placeholder='password' name="password" onChange={loginFormData} className='text-lg mb-10 h-10 p-2 outline-0 border-b-2 text-[#71b1e5] border-b-[#71b1e5]'/>
+              <input type='text' placeholder='email' name="email" value="q@q.com" onChange={loginFormData} className='text-lg mb-5 h-10 p-2 outline-0 border-b-2 text-[#71b1e5] border-b-[#71b1e5]'/>
+              <input type='text' placeholder='password' name="password" value="qqq" onChange={loginFormData} className='text-lg mb-10 h-10 p-2 outline-0 border-b-2 text-[#71b1e5] border-b-[#71b1e5]'/>
               <button className='cursor-pointer rounded self-start p-2 bg-[#71b1e5] text-white'>Sign In</button>
             </form>
             <p className='text-right text-[#71b1e5]'>Don't have account? <Link to="/signup" className='font-bold'>SignUp </Link> Now.</p>
